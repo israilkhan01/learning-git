1da4684e422aba1115adca4d962de63b41737dc1
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-03-11 23:37:23 +0530

    added scrolling features

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-var doc = document.getElementsByTagName("header");
+var navmenuselector=document.querySelectorAll(".nav-menu a");
+console.log(navmenuselector);
+for(var i=0;i<navmenuselector.length;i++){
+     navmenuselector[i].addEventListener('click',function(event){
+          event.preventDefault();
+          var targetSectionID=this.textContent.trim().toLowerCase();
+          console.log(targetSectionID);
+          var targetsection=document.getElementById(targetSectionID);
+          console.log(targetsection);
+          var interval=setInterval(function(){
+               var targetsectioncoordinates=targetsection.getBoundingClientRect();
+               if(targetsectioncoordinates.top<=0){
+                    clearInterval(interval);
+                    return;
+               }
 
-function fame(user){
-     console.log("hello world");   
+               else if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+                    clearInterval(interval);
+                    return;
+                    };
+               window.scrollBy(0,50);
+          },25);
+     });
 }
+var buttons=document.getElementById('mesg-button');
+buttons.addEventListener('click',function(event){
+     window.open('message.html')
+})
