da98555e22e92fe6c0eeaa8ef41c2d08e2967462
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-03-16 18:30:16 +0530

    updated font-styles

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
 var navmenuselector=document.querySelectorAll(".nav-menu a");
-console.log(navmenuselector);
-for(var i=0;i<navmenuselector.length;i++){
-     navmenuselector[i].addEventListener('click',function(event){
-          event.preventDefault();
-          var targetSectionID=this.textContent.trim().toLowerCase();
-          console.log(targetSectionID);
-          var targetsection=document.getElementById(targetSectionID);
-          console.log(targetsection);
-          var interval=setInterval(function(){
-               var targetsectioncoordinates=targetsection.getBoundingClientRect();
-               if(targetsectioncoordinates.top<=0){
-                    clearInterval(interval);
-                    return;
-               }
+function scrollfun(event){
+     event.preventDefault();
+     var targetSectionID=this.textContent.trim().toLowerCase();
+     var targetsection=document.getElementById(targetSectionID);
+     var interval=setInterval(function(){
+          var targetsectioncoordinates=targetsection.getBoundingClientRect();
+          if(targetsectioncoordinates.top<=0){
+               clearInterval(interval);
+               return;
+          }
 
-               else if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-                    clearInterval(interval);
-                    return;
-                    };
-               window.scrollBy(0,50);
-          },25);
-     });
+          else if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+               clearInterval(interval);
+               return;
+               };
+          window.scrollBy(0,50);
+     },25);
 }
+for(var i=0;i<navmenuselector.length;i++){
+     navmenuselector[i].addEventListener('click',scrollfun);
+}
+
 var buttons=document.getElementById('mesg-button');
 buttons.addEventListener('click',function(event){
      window.open('message.html')
 })
+
+$(document).ready(function(){
+     $(".Menu").click(function(){
+         $(".menu-button").slideToggle("fast");
+     });
+ });
+$(document).on("click", function(event){
+     var trigger = $(".Menu");
+     var wid=$(document).width();
+     if(trigger !== event.target && !trigger.has(event.target).length  ){
+         $(".menu-button").slideUp("fast");
+         console.log(trigger.has(event.target).length);
+     } 
+
+     
+ });
+
+ if($(document).width>750){
+      $(".horizontal-list").show();
+ }
